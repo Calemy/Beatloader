ec8c3519a8c52c5709e66a88b6c74222b7e68d33
Nanoo <45440100+Calemy@users.noreply.github.com>
2024-05-06 02:12:41 +0200

    qol: rework functions

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import { showOnDiscord, modes, status, custom, attributes, search as query, down
 
 const logger = new Logger()
 const client = new rpc.Client({ transport: "ipc" })
+let discordRPC = false;
 
 const time = new Date()
 let count = 0
@@ -16,6 +17,12 @@ let error = false;
 
 if(showOnDiscord){
     client.on("ready", () => {
+        discordRPC = true;
+        main()
+    })
+
+    client.on("error", () => {
+        logger.red("Discord RPC error").send()
         main()
     })
 
@@ -72,7 +79,7 @@ async function main(){
         await crawl()
     }
     logger.red("Reached end of downloads, terminating...").send()
-    process.exit(1)
+    return process.exit(1)
 }
 
 function crawl(){
@@ -80,10 +87,10 @@ function crawl(){
         const req = await fetch(`https://catboy.best/api/v2/search?q=${query}[${attributes}]&mode=${modes.join("&mode=")}&status=${status.join("&status=")}&limit=1000&offset=${offset}`)
         if(req.status != 200){
             if(req.status == 500){
-                logger.red("Something in your query is wrong, please report this to the developer.").send()
+                logger.red("Something in your query is wrong, please fix this or report this to a developer.").send()
                 return process.exit(1)
             }
-            logger.red(`catboy.best seems to not have given you a proper response, please report this to the developer. (${req.status})`).send()
+            logger.red(`catboy.best seems to not have given you a proper response, please report this to a developer. (${req.status})`).send()
             return process.exit(1)
         }
         const search = await req.json()
@@ -93,8 +100,8 @@ function crawl(){
         for(let i = 0; i < search.length; i++) {
             if(cache.indexOf(String(search[i].id)) != -1) continue;
             if(!custom(search[i])) continue;
-            await download(search[i])
-            await new Promise((r) => setTimeout(r, 5000))
+            const t = await download(search[i])
+            await new Promise((r) => setTimeout(r, t))
         }
 
         offset += search.length
@@ -105,98 +112,103 @@ function crawl(){
 
 function download(data, retries = 0){
     const id = String(data.id)
-    const info = rankedStatus(data.ranked)
+    const { color, tag } = rankedStatus(data.ranked)
     return new Promise(async (resolve) => {
         const start = Date.now()
         try {
             const file = await fetch(`https://catboy.best/d/${id}${downloadVideo ? "" : "n"}`)
-            const length = file.headers.get("content-length")
-            const type = file.headers.get("content-type")
-            if(type?.startsWith("application/json")){
-                const e = await file.json()
-                if(e?.error == "Ratelimit"){
-                    logger.red("Mirror reached ratelimit, pausing..").send()
-                    await new Promise((r) => setTimeout(r, 1000 * 60 * 10))
-                    return resolve(await download(data))
-                } else if (e?.error == "Map not available for download"){
-                    logger.white(`${id} | `)[info.color](`${info.tag} `)
-                    .white(`| ${data.artist} - ${data.title} (${data.creator}) | `)
-                    .red(`Failed`).send()
-                    return resolve()
-                } else {
-                    if(retries < 3) return resolve(download(data, retries++))
-                    logger.red("Something went wrong:").send()
-                    console.log(e)
+            if(!file.ok){
+                switch(file.status){
+                    case 425:
+                        logger.red("Mirror reached ratelimit, pausing...").send()
+                        return resolve(1000 * 60 * 10)
+                    case 429:
+                        logger.red("Waiting for ratelimit cooldown").send()
+                        await new Promise((r) => setTimeout(r, 1000 * 60))
+                        return resolve(await download(...arguments))
+                    case 451:
+                        logger.red("Map not available for download").send()
+                        return resolve(1)
+                    case 500:
+                        logger.red("Internal Server Error, stopping program to prevent further errors.").send()
+                        return process.exit(1)
+                    case 502:
+                        logger.red("Server offline, stopping program.").send()
+                        return process.exit(1)
+                    default:
+                        logger.red(`Something went wrong: ${file.status}`).send()
+                        return resolve(1)
                 }
-                return resolve()
             }
+            const length = file.headers.get("content-length")
+            const origin = file.headers.get("content-origin")
+            const timeout = (() => {
+                switch(origin){
+                    case "local":
+                        return 1000 * (60 / (30 / 1))
+                    case "s3":
+                        return 1000 * (60 / (30 / 3))
+                    case "bancho":
+                        return 1000 * (60 / (30 / 5))
+                }
+            })();
 
-            const fileStream = fs.createWriteStream(`./songs/${id}.osz`);
-            file.body.pipe(fileStream);
-
-            await new Promise((resolve) => {
-                fileStream.on("finish", async () => {
-                    const downloadTime = Date.now() - start
-
-                    const check = fs.statSync(`./songs/${id}.osz`) 
-                    if (check.size != length) {
-                        fs.rmSync(`./data/${id}.osz`)
-                        return resolve(await download(data))
-                    }
-                    size += check.size
-                    count++
-                    error = 0;
-                    fs.appendFileSync("./.data/completed", `${id},`)
-                    fs.appendFileSync("./.data/size", `${check.size},`)
-                    logger.white(`${id} | `)[info.color](`${info.tag} `)
-                    .white(`| ${data.artist} - ${data.title} (${data.creator}) | `)
-                    .green(`${(downloadTime / 1000).toFixed(2)} seconds (${(check.size / (1024 * 1024)).toFixed(2)}MB)`).send()
-                    updateActivity()
-                    resolve()
-                });
-
-                fileStream.on("error", (error) => {
-                    if(error.message == "ENOSPC: no space left on device, write"){
-                        logger.red("No space left, exiting program..").send()
-                        process.exit(1)
-                    }
-                })
+            const stream = fs.createWriteStream(`./songs/${id}.osz`);
+            file.body.pipe(stream);
+            file.body.once("error", () => {
+                fileStream.destroy()
+                fs.rmSync(`./songs/${id}.osz`)
             })
 
-            resolve()
+            stream.once("error", (e) => {
+                if(e.message == "ENOSPC: no space left on device, write"){
+                    logger.red("No space left, exiting program..").send()
+                    return process.exit(1)
+                }
+            })
 
+            stream.once("close", () => {
+                if(!stream.writableFinished) return resolve(timeout);
+                const downloadTime = Date.now() - start
+                size += Number(length)
+                count++
+                fs.appendFileSync("./.data/completed", `${id},`)
+                fs.appendFileSync("./.data/size", `${length},`)
+                logger.white(`${id.padStart(7, " ")} | `)[color](`${tag} `)
+                .white(`| ${data.artist} - ${data.title} (${data.creator}) | `)
+                .green(`${(downloadTime / 1000).toFixed(2)} seconds (${(length / (1024 * 1024)).toFixed(2)}MB)`).send()
+                updateActivity()
+                return resolve(timeout)
+            })
         } catch (error){
-            if(retries < 3) return resolve(download(data, retries++))
+            if(retries < 3) return resolve(download(data, ++retries))
             logger.red("Something went wrong:").send()
             console.log(error)
         }
     })
 }
 
-function updateActivity(start = false){
-    if(!showOnDiscord) return;
-
-    if(start){
-        client.setActivity({
-            details: "Running Beatloader v1",
-            state: "Starting up...",
-            startTimestamp: time,
-            instance: false
-        })
-        return;
-    }
-
-    client.setActivity({
-        details: "Downloading from catboy.best",
-        state: `${count} beatmaps (${(size / (1024 * 1024 * 1024)).toFixed(2)}GB)`,
+function updateActivity(start){
+    if(!discordRPC) return;
+    let activity = {
         startTimestamp: time,
         instance: false,
-        largeImageKey: "logo",
-        buttons: [
+    }
+
+    if(start){
+        activity.details = "Running Beatloader v1"
+        activity.state = "Starting up..."
+    } else {
+        activity.details = "Downloading from catboy.best",
+        activity.state = `${count} beatmaps (${(size / (1024 * 1024 * 1024)).toFixed(2)}GB)`
+        activity.largeImageKey = "logo"
+        activity.buttons = [
             { label: "Visit catboy.best", url: "https://catboy.best"},
             { label: "Download", url: "https://github.com/calemy/Beatloader"}
         ]
-    })
+    }
+
+    client.setActivity(activity);
 }
 
 function rankedStatus(status){
